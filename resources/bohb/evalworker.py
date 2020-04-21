71a2d15578fb905ed6ec511373f71f76dcfbdac0
wever <wever@pc-kb-felix.cs.uni-paderborn.de>
2020-04-21 16:43:05 +0200

    Clean up.

--- resources/bohb/evalworker.py
+++ resources/bohb/evalworker.py
@@ -14,8 +14,6 @@ class MyWorker(Worker):
     def __init__(self, *args, sleep_interval=0, gRPC_port, **kwargs):
         super().__init__(*args, **kwargs)
         self.gRPC_port = gRPC_port
-        self.log = open("worker.log", "a")
-        self.name = "w"
 
     def compute(self, config, budget, **kwargs):
         """
@@ -34,28 +32,21 @@ class MyWorker(Worker):
                 'loss' (scalar)
                 'info' (dict)
         """
+        
+        with open("worker.log", "a") as file:
+            file.write("Budget " + str(budget)+"\n")
 
-        self.log.write(self.name + ": Evaluate configuration " + str(config) + " with budget " + str(budget) +"\n")
         params = []
         for k, v in config.items():
             params.append(PCSBasedComponentParameter_pb2.PCSBasedParameterProto(key=k, value=str(v)))
-        cmp = PCSBasedComponentParameter_pb2.PCSBasedComponentProto(name=str(math.floor(budget)), parameters=params)
-        self.log.write(self.name + ": Built protobuf component " + str(cmp) +"\n")
+        cmp = PCSBasedComponentParameter_pb2.PCSBasedComponentProto(name=str(math.ceil(budget/1000)), parameters=params)
 
-        self.log.write(self.name + ": Connect to evaluation backend\n")
         channel = grpc.insecure_channel("localhost:" + str(self.gRPC_port))
         stub = PCSBasedComponentParameter_pb2_grpc.PCSBasedOptimizerServiceStub(channel)
 
 
-        self.log.write(self.name + ": Evaluate component \n")
         response = stub.Evaluate(cmp)
         channel.close()
-        self.log.write(self.name + ": Received response " + str(response) + " and closed the connection.\n")
-
-        # res = numpy.clip(config['x'] + numpy.random.randn() / budget, config['x'] / 2, 1.5 * config['x'])
-        # config['weka.classifiers.bayes.BayesNet.Q']
-        # time.sleep(self.sleep_interval)
-        self.log.write(self.name + ": Result " + str(response.result)+"\n")
 
         if(response.result < 0):
             return( {'loss': float(10000), 'info': 'crashed'})
--- resources/bohb/run.py
+++ resources/bohb/run.py
@@ -10,61 +10,48 @@ import hpbandster.core.nameserver as hpns
 import hpbandster.core.result as hpres
 
 from hpbandster.optimizers import BOHB as BOHB
+from hpbandster.optimizers import HyperBand as HyperBand
 from evalworker import MyWorker
 
-log = open("run.log", "a")
-log.write("Parse arguments\n")
-log.flush()
-parser = argparse.ArgumentParser(description='BOHB')
+parser = argparse.ArgumentParser(description='Hyperband')
 parser.add_argument('--min_budget', type=float, help='Minimum budget used during the optimization.', default=1)
 parser.add_argument('--max_budget', type=float, help='Maximum budget used during the optimization.', default=5)
-parser.add_argument('--n_iterations', type=int, help='Number of iterations performed by the optimizer', default=4)
+parser.add_argument('--eta', type=float, help='In each iteration, a complete run of sequential halving is executed. In it, after evaluating each configuraiton on the same subset size, only a fraction of 1/eta of them advances to the next round. Must be greater or equal to 2', default=2)
 parser.add_argument('--id', type=int, help='ID of this run', default=0)
 parser.add_argument('--n_workers', type=int, help='Number of workers to run in parallel', default=2)
 args = parser.parse_args()
 
-log.write("Start runner\n")
-log.flush()
 # Step 1: Start a nameserver
 # Every run needs a nameserver. It could be a 'static' server with a
 # permanent address, but here it will be started for the local machine with the default port.
 # The nameserver manages the concurrent running workers across all possible threads or clusternodes.
 # Note the run_id argument. This uniquely identifies a run of any HpBandSter optimizer.
 runID='hb'+str(args.id)
-log.write("Run ID is " + runID + ". Now create nameserver\n")
-log.flush()
-NS = hpns.NameServer(run_id=runID, host='127.0.0.1', port=None)
-NS.start()
-log.write("Created nameserver with runID " + runID +"\n")
-log.flush()
-
 with open('client.conf') as json_file:
     config = json.load(json_file)
 gRPC_port = str(config['gRPC_port']);
-    
+
+NS = hpns.NameServer(run_id=runID, host='127.0.0.1', port=None)
+NS.start()
+
 workers = []
 for i in range(args.n_workers):
     w = MyWorker(gRPC_port=gRPC_port, nameserver='127.0.0.1', run_id=runID, id=i)
     w.run(background=True)
     workers.append(w)
     
+with open("run.log", "a") as file:
+    file.write("Eta " + str(args.eta)+"\n")
+    
 optimizer = BOHB(configspace=MyWorker.get_configspace(),
-    run_id=runID, nameserver='127.0.0.1',
+    run_id=runID, nameserver='127.0.0.1', eta=args.eta,
     min_budget=args.min_budget, max_budget=args.max_budget)
-log.write("created optimizer, now let it run\n")
-log.flush()
-res = optimizer.run(n_iterations=args.n_iterations, min_n_workers=args.n_workers)
+res = optimizer.run(min_n_workers=args.n_workers)
 
 # Step 4: Shutdown
 # After the optimizer run, we must shutdown the master and the nameserver.
-log.write("shutdown optimizer\n")
-log.flush()
 optimizer.shutdown(shutdown_workers=True)
-log.write("optimizer shut down\n")
-log.flush()
 NS.shutdown()
-log.write("Shut down nameserver\n")
-log.flush()
 
 # Step 5: Analysis
 # Each optimizer returns a hpbandster.core.result.Result object.
