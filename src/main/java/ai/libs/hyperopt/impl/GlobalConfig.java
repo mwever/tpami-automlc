0d95e9141b684cef2edd70728f4189db71bf9340
mwever <wever@mail.uni-paderborn.de>
2020-04-21 16:21:37 +0200

    Clean up.

--- src/main/java/ai/libs/hyperopt/impl/GlobalConfig.java
+++ src/main/java/ai/libs/hyperopt/impl/GlobalConfig.java
@@ -1,15 +1,7 @@
 package ai.libs.hyperopt.impl;
 
-import java.io.File;
-import java.util.Arrays;
-
-import ai.libs.jaicore.basic.sets.SetUtil;
-
 public class GlobalConfig {
 
-	public static final String OPTIMIZER_CONFIG_PATH = "conf/smac-optimizer-config.properties";
-
-//	public static final String PYTHON_EXEC = SetUtil.implode(Arrays.asList("C:" + File.separator, "Users", "Marcel", "AppData", "Local", "Programs", "Python", "Python38-32", "python.exe"), File.separator);
 	public static final String PYTHON_EXEC = "python";
 
 }
